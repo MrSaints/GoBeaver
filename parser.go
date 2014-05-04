f9b0b3c8b85eda0eb29cda88a1219612f4910355
MrSaints <ian@fyianlai.com>
2014-05-05 00:42:05 +0100

    Added resolved URL; changed variables

--- parser.go
+++ parser.go
@@ -2,21 +2,18 @@ package main
 
 import (
     "log"
-    "fmt"
     "strings"
+    "net/url"
     "github.com/PuerkitoBio/goquery"
 )
 
-var BASE_URL = "http://www.lse.ac.uk"
-var COURSES_URL = map[string]string {
-    "Undergraduate":    BASE_URL + "/resources/calendar/courseGuides/undergraduate.htm",
-    "Graduate":         BASE_URL + "/resources/calendar/courseGuides/graduate.htm",
-    "Research":         BASE_URL + "/resources/calendar/courseGuides/research.htm",
+const LSE_URL = "http://www.lse.ac.uk"
+var PROGRAMMES_URL = map[string]string {
+    "Undergraduate":    LSE_URL + "/resources/calendar/courseGuides/undergraduate.htm",
+    "Graduate":         LSE_URL + "/resources/calendar/courseGuides/graduate.htm",
+    "Research":         LSE_URL + "/resources/calendar/courseGuides/research.htm",
 }
 
-type Undergraduate []*Course
-type Graduate []*Course
-type Research []*Course
 type Course struct {
     Code string
     Title string
@@ -25,22 +22,29 @@ type Course struct {
     Students int
     Class int
     Value int
+    Program int
 }
 
 func main() {
     var program *goquery.Document
     var e error
 
-    if program, e = goquery.NewDocument(COURSES_URL["Undergraduate"]); e != nil {
+    if program, e = goquery.NewDocument(PROGRAMMES_URL["Undergraduate"]); e != nil {
         log.Fatal(e)
     }
 
+    var courses []interface{}
     program.Find("table tr td p a").Each(func(i int, s *goquery.Selection) {
         course_item := strings.Split(s.Text(), " ")
+        course_item_url, _ := s.Attr("href")
+        parsed_url, _ := url.Parse(PROGRAMMES_URL["Undergraduate"])
+        parsed_relative, _ := url.Parse(course_item_url)
+
         course_object := new(Course)
         course_object.Code = course_item[0]
         course_object.Title = course_item[1]
-        course_object.URL, _ = s.Attr("href")
-        fmt.Printf("%s: %s (%s)\n", course_object.Code, course_object.Title, course_object.URL)
+        course_object.URL = parsed_url.ResolveReference(parsed_relative).String()
+        course_object.Program = 0
+        courses = append(courses, course_object)
     })
 }
