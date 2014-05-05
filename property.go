a93799904241fda1959ef41f0c4741e550fb8135
MrSaints <ian@fyianlai.com>
2014-05-06 00:53:11 +0100

    Added getter methods

---
+++ property.go
@@ -0,0 +1,34 @@
+package main
+
+func (this *Course) Code() string {
+	return this.code
+}
+
+func (this *Course) Title() string {
+	return this.title
+}
+
+func (this *Course) URL() string {
+	return this.url
+}
+
+func (this *Course) Department() string {
+	return this.department
+}
+
+func (this *Course) Students() int {
+	return this.students
+}
+
+func (this *Course) Class() int {
+	return this.class
+}
+
+func (this *Course) Value() int {
+	return this.value
+}
+
+func (this *Course) Program() int {
+	return this.program
+}
+
