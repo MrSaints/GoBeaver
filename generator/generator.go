80c32711cd56d6f1e5c766d84c20a118adef4b8c
MrSaints <ian@fyianlai.com>
2014-05-07 09:04:39 +0100

    Updated generator to dump all undergraduate course data

--- generator/generator.go
+++ generator/generator.go
@@ -2,37 +2,26 @@ package main
 
 import (
     "encoding/json"
+    "io/ioutil"
     "log"
-    "sort"
     "github.com/MrSaints/beaverguide"
 )
 
 func main() {
-    // TODO: Implement concurrency
-
-    var courses beaverguide.Courses
-    for program, _ := range beaverguide.PROGRAMMES_URL {
-        courses = append(courses, beaverguide.GetCourses(program)...)
-    }
-    sort.Sort(courses)
+    courses := beaverguide.GetCourses("Undergraduate")
+    //courses := beaverguide.GetAllCourses()
     log.Printf("Total courses: %d", len(courses))
+    courses.GetProperties()
 
-    // Test
-    for _, test_c := range courses {
-        log.Print(test_c)
-        test_c.GetProperties()
-        /*log.Print(test_c.Teachers())
-        log.Print(test_c.Availability())
-        log.Print(test_c.Content())
-        log.Print(test_c.Teaching())
-        log.Print(test_c.Formative())
-        log.Print(test_c.Readings())
-        log.Print(test_c.Assessments())*/
-        test_json, e := json.Marshal(test_c)
-        if e != nil {
-            log.Fatal(e)
-        }
-        log.Print(string(test_json))
-        break
+    json_courses, json_error := json.Marshal(courses)
+    if json_error != nil {
+        log.Fatal(json_error)
     }
+
+    file_error := ioutil.WriteFile("courses_dump.json", json_courses, 0644)
+    if file_error != nil {
+        panic(file_error)
+    }
+
+    log.Print("Complete!")
 }
